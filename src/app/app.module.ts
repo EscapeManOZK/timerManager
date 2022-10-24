ffadf3a52b9c72b918e7518a559f96d245680508
EscapeManOZK <romowczarek@gmail.com>
2022-10-24 18:45:05 +0200

    First app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,64 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconsProviderModule } from './shared/lib/icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { DemoNgZorroAntdModule } from './shared/lib/ng-zorro-antd.module';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { BreadcrumbComponent } from './layout/breadcrumb/breadcrumb.component';
+
+registerLocaleData(en);
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    BreadcrumbComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    DemoNgZorroAntdModule,
+    BrowserAnimationsModule,
+    IconsProviderModule,
+    ScrollingModule,
+    DragDropModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
+  bootstrap: [AppComponent],
+  exports: [
+    BreadcrumbComponent
+  ]
 })
 export class AppModule { }
